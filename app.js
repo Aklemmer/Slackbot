c8baaf3d33e84fb8af6cb2c35e86b2372235d94c
Aklemmer <Arklemmer@gmail.com>
2018-06-02 17:24:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,34 @@ var dotenv        = require('dotenv');
 var SpotifyWebApi = require('spotify-web-api-node');
 
 dotenv.load();
+var clientId     : process.env.SPOTIFY_KEY;
+  var  clientSecret : process.env.SPOTIFY_SECRET;
+  var redirectUri  : process.env.SPOTIFY_REDIRECT_URI;
+var userID = SPOTIFY_USERNAME;
+var playlistID = process.env.SPOTIFY_PLAYLIST_ID;
 
+function slack(res, message) {
+  if (process.env.SLACK_OUTGOING === 'true') {
+    return res.send(JSON.stringify({text: message}));
+  } else {
+    return res.send(message);
+  }
+}
 
-var spotifyApi = new SpotifyWebApi({
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function(req, res) {
+  if (spotifyApi.getAccessToken()) {
+    return res.send('You are logged in.');
+  }
+  return res.send('<a href="/authorise">Authorise</a>');
+});
+
+/*var spotifyApi = new SpotifyWebApi({
   clientId     : process.env.SPOTIFY_KEY,
   clientSecret : process.env.SPOTIFY_SECRET,
   redirectUri  : process.env.SPOTIFY_REDIRECT_URI
@@ -86,7 +111,36 @@ app.post('/store', function(req, res) {
     }, function(err) {
       return slack(res, 'Could not refresh access token. You probably need to re-authorise yourself from your app\'s homepage.');
     });
-});
+});*/
+
+
+	var doLogin = function(callback) {
+		var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
+			'&response_type=token' +
+			'&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
+			'&redirect_uri=' + encodeURIComponent(redirect_uri);
+		localStorage.setItem('createplaylist-tracks', JSON.stringify(g_tracks));
+		localStorage.setItem('createplaylist-name', g_name);
+		var w = window.open(url, 'asdf', 'WIDTH=400,HEIGHT=500');
+	}
+ 
+  var text = process.env.SLACK_OUTGOING === 'true' ? req.body.text.replace(req.body.trigger_word, '') : req.body.text;
+
+  var track = substringAfter(text, "track/");
+
+  var response = 'success';
+
+  var addTrack = function (track, callback) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks?' + 'uris='+ 'spotify%' + track,
+        success: function (response) {
+            callback(response);
+        }
+    });
+};
+
+app.dologin();
+app.addTrack(track, callback);
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
